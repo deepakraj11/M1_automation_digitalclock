accd38c4433f96c80514ebcab9e3fb224b70f524
deepakraj11 <94216191+deepakraj11@users.noreply.github.com>
2021-11-23 22:30:50 +0530

    Update automation.h

--- 3_IMPLEMENTATION/inc/automation.h
+++ 3_IMPLEMENTATION/inc/automation.h
@@ -9,8 +9,23 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#ifndef __AUTOMATION_H
-#define __AUTOMATION_H
-int date();
-int tinme();
-#endif
+#define RESET		0
+#define BRIGHT 		1
+
+#define BLACK 		0
+#define RED		1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+#define DONT_CARE	8
+
+void textcolor(int,int,int);
+int input_colour(char *);
+int identify_digit(char);
+void fix_digit(char, int, int);
+int block_position(int );
+void print_digit(int (*array)[6], int, int);
+void print_colon(int, int);
